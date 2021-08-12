b033cbc73ced28f46fddf13a67e3b1338c69a796
godfunc <godfunc@outlook.com>
2021-08-12 21:31:39 +0800

    修复ID冲突问题

--- common/src/main/java/com/godfunc/config/MybatisPlusConfig.java
+++ common/src/main/java/com/godfunc/config/MybatisPlusConfig.java
@@ -1,20 +1,29 @@
 package com.godfunc.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.incrementer.DefaultIdentifierGenerator;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * mybatis-plus 配置
+ *
  * @author godfunc
  * @email godfunc@outlook.com
  */
 @Configuration
 @MapperScan(basePackages = "com.godfunc.**.mapper")
 public class MybatisPlusConfig {
+
+    @Value("${mybatis-plus.global-config.workerId}")
+    private Long workerId;
+
+    @Value("${mybatis-plus.global-config.dataCenterId}")
+    private Long dataCenterId;
 
     /**
      * 分页插件
@@ -24,5 +33,10 @@ public class MybatisPlusConfig {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
+    }
+
+    @Bean
+    public DefaultIdentifierGenerator defaultIdentifierGenerator() {
+        return new DefaultIdentifierGenerator(workerId, dataCenterId);
     }
 }

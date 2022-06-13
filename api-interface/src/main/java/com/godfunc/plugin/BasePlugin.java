dfe85c74a991bc2a472256d22f697222c1221e9a
godfunc <godfunc@outlook.com>
2022-06-13 22:05:18 +0800

    add BasePlugin

---
+++ api-interface/src/main/java/com/godfunc/plugin/BasePlugin.java
@@ -0,0 +1,46 @@
+package com.godfunc.plugin;
+
+import com.godfunc.dto.PayInfoDTO;
+import com.godfunc.entity.OrderTemp;
+import org.pf4j.Plugin;
+import org.pf4j.PluginWrapper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public abstract class BasePlugin extends Plugin {
+    /**
+     * Constructor to be used by plugin manager for plugin instantiation.
+     * Your plugins have to provide constructor with this exact signature to
+     * be successfully loaded by manager.
+     *
+     * @param wrapper
+     */
+    public BasePlugin(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    /**
+     * create order
+     * @param order
+     * @return
+     */
+    public abstract PayInfoDTO doPay(OrderTemp order);
+
+    /**
+     * default handler response
+     * @param payInfo
+     * @param request
+     * @param response
+     */
+    public void handleResponse(PayInfoDTO payInfo, HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.sendRedirect(payInfo.getPayUrl());
+        } catch (IOException e) {
+            log.error("跳转到支付链接异常", e);
+        }
+    }
+
+
+}
---
+++ pay-plugins/normalPayPlugin/src/main/java/com/godfunc/plugin/NormalPayPlugin.java
@@ -0,0 +1,73 @@
+package com.godfunc.plugin;
+
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayConfig;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.domain.AlipayTradePrecreateModel;
+import com.alipay.api.request.AlipayTradePrecreateRequest;
+import com.alipay.api.response.AlipayTradePrecreateResponse;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.godfunc.dto.PayInfoDTO;
+import com.godfunc.entity.OrderDetailTemp;
+import com.godfunc.entity.OrderTemp;
+import org.pf4j.PluginWrapper;
+
+import java.util.Map;
+
+public class NormalPayPlugin extends BasePlugin {
+    /**
+     * Constructor to be used by plugin manager for plugin instantiation.
+     * Your plugins have to provide constructor with this exact signature to
+     * be successfully loaded by manager.
+     *
+     * @param wrapper
+     */
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public NormalPayPlugin(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public PayInfoDTO doPay(OrderTemp order) {
+        OrderDetailTemp detail = order.getDetail();
+        AlipayConfig alipayConfig = new AlipayConfig();
+        alipayConfig.setServerUrl(detail.getChannelCreateUrl());
+        alipayConfig.setAppId(detail.getPayChannelAccountCode());
+        Map<String, String> keyInfoMap = null;
+        try {
+            keyInfoMap = objectMapper.readValue(detail.getPayChannelAccountKeyInfo(), new TypeReference<Map<String, String>>() {
+            });
+        } catch (Exception e) {
+            log.error("解析渠道账号密钥信息异常 keyInfo={}, {}", detail.getPayChannelAccountKeyInfo(), e);
+        }
+
+        alipayConfig.setPrivateKey(keyInfoMap.get("privateKey"));
+        alipayConfig.setAppCertContent(keyInfoMap.get("appCertContent"));
+        alipayConfig.setAlipayPublicCertContent(keyInfoMap.get("alipayPublicCertContent"));
+        alipayConfig.setRootCertContent(keyInfoMap.get("rootCertContent"));
+        try {
+            DefaultAlipayClient defaultAlipayClient = new DefaultAlipayClient(alipayConfig);
+            AlipayTradePrecreateRequest request = new AlipayTradePrecreateRequest();
+            AlipayTradePrecreateModel model = new AlipayTradePrecreateModel();
+            model.setOutTradeNo(order.getOrderNo());
+            // TODO
+            //model.setTotalAmount(AmountUtil.convertCent2Dollar(order.getAmount()));
+            model.setSubject(detail.getGoodName());
+            model.setBody(detail.getGoodName());
+            request.setBizModel(model);
+            AlipayTradePrecreateResponse response = defaultAlipayClient.certificateExecute(request);
+            if ("10000".equals(response.getCode())) {
+                return new PayInfoDTO(null, response.getQrCode());
+            } else {
+                // TODO
+                //throw new GException("上游错误 {}", response.getSubMsg());
+            }
+        } catch (AlipayApiException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
